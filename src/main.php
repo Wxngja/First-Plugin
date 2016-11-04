77cb2a8f02a170e321ba17ebd7658d2d0be00144
Jonathanftw <romerojonathan1000@gmail.com>
2016-11-04 16:16:09 -0700

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,1 +1,13 @@
+<$php
+
+namespace Test;
+
+pocketmine\plugin\PluginBase;
+pocketmine\event\Listener;
+
+class Test extends PluginBase implements Listener {
+
+   public function onEnable(){
+     $this->getLogger()->info("Plugin Loaded");
+   }
 
